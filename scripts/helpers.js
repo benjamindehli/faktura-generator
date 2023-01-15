7738f3c9724a31e01ff294c6476b925da646e3ac
Benjamin Dehli <superelg@gmail.com>
2023-01-15 20:21:22 +0100

    Add helper functions

---
+++ scripts/helpers.js
@@ -0,0 +1,34 @@
+const formatTextDateString = (date) => {
+    var d = new Date(date);
+    if (!!d.valueOf()) {
+        const year = d.getFullYear();
+        const month = d.getMonth() + 1 < 10 ? `0${d.getMonth() + 1}` : `${d.getMonth() + 1}`;
+        const day = d.getDate() < 10 ? `0${d.getDate()}` : `${d.getDate()}`;
+        return `${day}.${month}.${year}`;
+    } else {
+        console.warn("Invalid date");
+        return "";
+    }
+};
+
+const formatInputDateString = (date) => {
+    var d = new Date(date);
+    if (!!d.valueOf()) {
+        const year = d.getFullYear();
+        const month = d.getMonth() + 1 < 10 ? `0${d.getMonth() + 1}` : `${d.getMonth() + 1}`;
+        const day = d.getDate() < 10 ? `0${d.getDate()}` : `${d.getDate()}`;
+        return `${year}-${month}-${day}`;
+    } else {
+        console.warn("Invalid date");
+        return "";
+    }
+};
+
+const createPlaceholderOptionElement = (placeholderText) => {
+    const placeholderOptionElement = document.createElement("option");
+    placeholderOptionElement.disabled = true;
+    placeholderOptionElement.selected = true;
+    placeholderOptionElement.label = placeholderText;
+    placeholderOptionElement.innerText = placeholderText;
+    return placeholderOptionElement;
+}
